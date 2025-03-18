30b18c71237ee16307d0a86279e1e3837a194956
TrueLizardmaster <154645238+TrueLizardmaster@users.noreply.github.com>
2025-03-18 09:48:17 -0500

    Implemented Player Placeholder Animations

--- Player/player.gd
+++ Player/player.gd
@@ -1,14 +1,18 @@
 extends CharacterBody2D
 
-@onready var animated_sprite = 
-
-const SPEED : int = 500
+@onready var animated_sprite = $Sprite2D
+@onready var aniTree : AnimationTree = $AnimationTree
 var direction: Vector2 = Vector2.ZERO
+const SPEED : int = 500
 
+func _ready():
+	aniTree.active = true
+
+func _process(_delta):
+	update_animation_parameters()
 
 
 func _physics_process(delta):
-
 
 	if Input.is_action_just_pressed("Hide"):
 		print("Ahh so scary I must hide Aaaahhhh")
@@ -21,13 +25,43 @@ func _physics_process(delta):
 
 
 	# Get the input direction and handle the movement/deceleration.
-	direction = Input.get_vector("Left", "Right", "Up", "Down").normalize()
+	direction = Input.get_vector("Left", "Right", "Up", "Down").normalized()
 	if Input.is_action_just_pressed("Left") || Input.is_action_just_pressed("Right"):
 		direction.y=0
 	elif Input.is_action_just_pressed("Up") || Input.is_action_just_pressed("Down"):
 		direction.x=0
-	else:
-		direction = Vector2.ZERO
+	##else:
+	##	direction = Vector2.ZERO
 
 	velocity = direction * SPEED
 	move_and_slide()
+
+
+func update_animation_parameters():
+	if velocity == Vector2.ZERO:
+		aniTree["parameters/conditions/idle"] = true
+		aniTree["parameters/conditions/is_moving"] = false
+		
+	else:
+		aniTree["parameters/conditions/idle"] = false
+		aniTree["parameters/conditions/is_moving"] = true
+	
+	if (Input.is_action_just_pressed("Grab")):
+		aniTree["parameters/conditions/yoink"] = true
+	else:
+		aniTree["parameters/conditions/yoink"] = false
+
+	if Input.is_action_just_pressed("Push") || Input.is_action_just_pressed("Interact"):
+		aniTree["parameters/conditions/push"] = true
+	else:
+		aniTree["parameters/conditions/push"] = false
+		
+	if Input.is_action_just_pressed("Hide"):
+		aniTree["parameters/conditions/hiding"] = true
+	else:
+		aniTree["parameters/conditions/hiding"] = false
+		
+	aniTree["parameters/Idle/blend_position"] = direction
+	aniTree["parameters/Walk/blend_position"] = direction
+	aniTree["parameters/Grab/blend_position"] = direction
+	aniTree["parameters/Push/blend_position"] = direction
