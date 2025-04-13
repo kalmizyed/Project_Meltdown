70dfea215e6a79a3ab49259453e1f226f8d6a5ad
em9500 <emerson.chism@gmail.com>
2025-04-13 17:15:38 -0500

    Moved Inventory to Screens folder + bug fixes

--- scripts/entities/player_placeholder.gd
+++ scripts/entities/player_placeholder.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @export var SPEED: float = 100.0
+@onready var timer2 = $Timer2
 
 func _physics_process(_delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
@@ -14,8 +15,8 @@ func _physics_process(_delta: float) -> void:
 	velocity.y = direction['y'] * SPEED if direction['y'] else move_toward(velocity.y, 0, SPEED)
 	
 	if(velocity.length()!=0):
-		if(%Timer.time_left<=0):
+		if($Timer2.time_left<=0):
 			AudioController.play_footsteps_with_reverb()
-			$Timer.start(0.75)
+			$Timer2.start(0.75)
 	
 	move_and_slide()
