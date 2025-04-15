5661169d5d9395b7729a4f8acec9b56ac4d39d1e
Kaden Almizyed <kalmizyed@wisc.edu>
2025-04-15 00:47:15 -0500

    Switch to real player scene, resize player scene, and fix input casing

--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @onready var animated_sprite = $Sprite2D
 @onready var aniTree : AnimationTree = $AnimationTree
 var direction: Vector2 = Vector2.ZERO
-const SPEED : int = 500
+const SPEED : int = 100
 
 func _ready():
 	aniTree.active = true
@@ -14,24 +14,24 @@ func _process(_delta):
 
 func _physics_process(delta):
 
-	if Input.is_action_just_pressed("Hide"):
-		print("Ahh so scary I must hide Aaaahhhh")
-	
-	if Input.is_action_just_pressed("Interact"):
-		print("Interaction oh shi waht's this?")
-		
-	if Input.is_action_just_pressed("Grab"):
-		print("Ohh Shiny dis shi is mine")
-		
-	if Input.is_action_just_pressed("Flashlight"):
-		print("The power of the sun the the palm of my hand")
+	#if Input.is_action_just_pressed("hide"):
+		#print("Ahh so scary I must hide Aaaahhhh")
+	#
+	#if Input.is_action_just_pressed("interact"):
+		#print("Interaction oh shi waht's this?")
+		#
+	#if Input.is_action_just_pressed("grab"):
+		#print("Ohh Shiny dis shi is mine")
+		#
+	#if Input.is_action_just_pressed("flashlight"):
+		#print("The power of the sun the the palm of my hand")
 
 
 	# Get the input direction and handle the movement/deceleration.
-	direction = Input.get_vector("Left", "Right", "Up", "Down").normalized()
-	if Input.is_action_just_pressed("Left") || Input.is_action_just_pressed("Right"):
+	direction = Input.get_vector("left", "right", "up", "down").normalized()
+	if Input.is_action_just_pressed("left") || Input.is_action_just_pressed("right"):
 		direction.y=0
-	elif Input.is_action_just_pressed("Up") || Input.is_action_just_pressed("Down"):
+	elif Input.is_action_just_pressed("up") || Input.is_action_just_pressed("down"):
 		direction.x=0
 	##else:
 		##direction = Vector2.ZERO
@@ -49,20 +49,21 @@ func update_animation_parameters():
 		aniTree["parameters/conditions/idle"] = false
 		aniTree["parameters/conditions/is_moving"] = true
 	
-	if (Input.is_action_just_pressed("Grab")):
-		aniTree["parameters/conditions/yoink"] = true
-	else:
-		aniTree["parameters/conditions/yoink"] = false
+	#if (Input.is_action_just_pressed("grab")):
+		#aniTree["parameters/conditions/yoink"] = true
+	#else:
+		#aniTree["parameters/conditions/yoink"] = false
 
-	if Input.is_action_just_pressed("Push") || Input.is_action_just_pressed("Interact"):
+	#if Input.is_action_just_pressed("push") || Input.is_action_just_pressed("interact"):
+	if Input.is_action_just_pressed("interact"):
 		aniTree["parameters/conditions/push"] = true
 	else:
 		aniTree["parameters/conditions/push"] = false
 		
-	if Input.is_action_just_pressed("Hide"):
-		aniTree["parameters/conditions/hiding"] = true
-	else:
-		aniTree["parameters/conditions/hiding"] = false
+	#if Input.is_action_just_pressed("hide"):
+		#aniTree["parameters/conditions/hiding"] = true
+	#else:
+		#aniTree["parameters/conditions/hiding"] = false
 		
 	aniTree["parameters/Idle/blend_position"] = direction
 	aniTree["parameters/Walk/blend_position"] = direction
