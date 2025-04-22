ac897b2cabfc1489cd5349bf576b51cad2ed3e4e
kalmizyed <kalmizyed@wisc.edu>
2025-04-22 13:14:41 -0500

    Update door logic and add option for them to require certain items

--- scripts/objects/door.gd
+++ scripts/objects/door.gd
@@ -12,7 +12,18 @@ var switchStates: Dictionary
 ## Make sure each required switch node has a unique name for this to work properly.
 @export var allSwitchesRequired: bool
 
+## Any items required to be able to open the door by hand, if any.
+@export var items: Array[Node]
+
 var isOpen: bool = false
+
+func passes_item_requirements() -> bool:
+	if items.size() > 0:
+		for item in items:
+			if item not in InventorySystem.items:
+				print('NOT FOUND: ' + str(item))
+				return false
+	return true
 
 func passes_requirements() -> bool:
 	for enabled in switchStates.values():
@@ -24,30 +35,31 @@ func _switch_flipped(name, state) -> void:
 	switchStates[name] = state
 	
 	# Handle opening/closing the door depending on switch rules
+	handle_open()
+
+func handle_open() -> void:
 	if allSwitchesRequired:
 		if passes_requirements():
 			open_door()
 		else:
 			close_door()
 	else:
-		handle_open()
-
-func handle_open() -> void:
-	AudioController.play_door()
-	if isOpen:
-		close_door()
-	else:
-		open_door()
+		if isOpen:
+			close_door()
+		else:
+			open_door()
 	$StaticBody2D/CollisionShape2D.disabled = isOpen
 
 func open_door() -> void:
 	if not isOpen:
 		$AnimatedSprite2D.play()
+		AudioController.play_door()
 		isOpen = true
 
 func close_door() -> void:
 	if isOpen:
 		$AnimatedSprite2D.play_backwards()
+		AudioController.play_door()
 		isOpen = false
 
 # Called when the node enters the scene tree for the first time.
@@ -57,6 +69,11 @@ func _ready() -> void:
 			if child is SwitchInteractable:
 				child.switch_flipped.connect(_switch_flipped)
 				switchStates[switch.name] = child.state
+	if allSwitchesRequired:
+		if passes_requirements():
+			open_door()
+		else:
+			close_door()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -65,5 +82,5 @@ func _process(delta: float) -> void:
 
 
 func _on_interactable_interacted() -> void:
-	if canOpen:
+	if canOpen and passes_item_requirements():
 		handle_open()
