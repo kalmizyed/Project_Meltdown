d682b4d6f7d1ee26c2e65c06d3effe50f906f699
Fiona Seto <fvseto@wisc.edu>
2025-04-25 18:10:36 -0500

    ui stuff

---
+++ scenes/screens/text_2.gd
@@ -0,0 +1,14 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if InventorySystem.is_collected("Diary2"):
+		set_disabled(false)
+	else:
+		set_disabled(true) # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- scripts/inventory_system.gd
+++ scripts/inventory_system.gd
@@ -1,6 +1,11 @@
 extends Node
 
 var items: Array[Node2D]
+var collected: Array[String] = []
 
 func on_item_collected(item: Node2D):
 	items.append(item)
+	collected.append(item.name)
+	
+func is_collected(itemName: String) -> bool:
+	return itemName in collected
