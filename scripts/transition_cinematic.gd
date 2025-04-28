d2e50ae8f95ab906ee66ec37b7b4faf7d68655d3
em9500 <emerson.chism@gmail.com>
2025-04-27 22:46:49 -0500

    Added subtitle file and started work on transition cinematic

--- scripts/transition_cinematic.gd
+++ scripts/transition_cinematic.gd
@@ -3,11 +3,25 @@ extends Node2D
 @onready var progress_bar = $ProgressBar
 @onready var rich_text_label = $RichTextLabel
 var time_taken = 0
-var END_TIME = 65
+var END_TIME = 34
+var level_instance : Node2D
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	AudioController.play_cinematic("robbie_cant_find_worker")
+	await get_tree().create_timer(36.0).timeout
+	load_level("Emerson_left_wing")
+	AudioController.play_level1()
+
+func unload_level():
+	if (is_instance_valid(level_instance)):
+		level_instance.queue_free()
+	level_instance = null
+	
+func load_level(level_name : String):
+	unload_level()
+	var level_path := "res://scenes/screens/%s.tscn" % level_name
+	var level_resource := load(level_path)
+	if(level_resource):
+		get_tree().change_scene_to_file(level_path)
 
 func sec_to_time(secs: int) -> String:
 	var return_val = ""
@@ -24,9 +38,6 @@ func sec_to_time(secs: int) -> String:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	time_taken += delta
-	if(time_taken<END_TIME):
+	if(time_taken<=END_TIME+1):
 		$ProgressBar.value = time_taken
-		$RichTextLabel.text = sec_to_time(time_taken) + "/1:05"
-	#else:
-		#if(time_taken-2>=END_TIME): #wait 2 seconds then switch scenes
-			#load next level
+		$RichTextLabel.text = sec_to_time(time_taken) + "/0:34"
