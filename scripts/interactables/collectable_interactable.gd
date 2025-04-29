d7e1fc56b9c2513c6891e18bfcc594bd54757c61
Kaden Almizyed <kalmizyed@wisc.edu>
2025-04-29 01:05:10 -0500

    Remove previously-collected items when loading a scene

--- scripts/interactables/collectable_interactable.gd
+++ scripts/interactables/collectable_interactable.gd
@@ -9,12 +9,17 @@ signal item_collected(item: Node2D)
 ## The type of collectable this CollectableInteractable is (diary or paper)
 @export var item_type: String
 
+func remove_item() -> void:
+	item_node.get_parent().remove_child.call_deferred(item_node)
+
 func _ready() -> void:
 	item_collected.connect(InventorySystem.on_item_collected)
 	if not item_node: item_node = self.get_parent()
+	if InventorySystem.is_collected(item_node.name):
+		remove_item()
 
 func _interact() -> void:
 	print("Item collected:", item_node.name)
 	AudioController.play_misc_pickup()
 	item_collected.emit(item_node)
-	item_node.get_parent().remove_child(item_node)
+	remove_item()
